df66f586d6bad247f096c857435d40ca4071ca6b
doubley318 <95672897+doubley318@users.noreply.github.com>
2022-05-25 22:34:16 +0800

    vision2.0
    优化了性能

--- study_time.cpp
+++ study_time.cpp
@@ -12,11 +12,14 @@ int main()
 {
     POINT p;
     POINT last_p;
+    // 定义总的学习时间：以分钟为单位
     int study_time = 0;
     time_t timePointer;
     time(&timePointer);
     char temp[64];
+    // 将时间转化为年月日时分秒的格式
     strftime(temp, sizeof(temp), "%Y%m%d_%H%M%S", localtime(&timePointer));
+    // 记录时间的文件
     string record_txt = (string)temp + ".txt";
     ofstream outfile(record_txt);
     if (!outfile)
@@ -32,36 +35,42 @@ int main()
         char temp[64];
         strftime(temp, sizeof(temp), "%Y-%m-%d %H:%M:%S", localtime(&timePointer));
 
-        GetCursorPos(&last_p); //获取鼠标坐标
+        GetCursorPos(&last_p); //获取鼠标起始坐标
 
         time_t now, later;
         time(&now);
         time(&later);
-        bool kerboard = false;
+        bool keyboard = false;
+        bool mouse = false;
+        // 每60秒检测一次
         while (later - now < 60)
         {
+            // 检测鼠标位置
+            GetCursorPos(&p); //获取鼠标结束坐标
+            if (!(p.x == last_p.x && p.y == last_p.y))
+                mouse = true;
+            // 检测键盘输入
             for (int i = 1; i < 255; i++)
             {
                 if (KEY_DOWN(i))
                 {
-                    kerboard = true;
-                    Sleep(1000 * (59 + now - later));
+                    keyboard = true;
                     break;
                 }
             }
+            if (keyboard || mouse)
+                Sleep(1000 * (59 + now - later));
             Sleep(1000);
             time(&later);
         }
 
-        GetCursorPos(&p); //获取鼠标坐标
-
-        if (!(p.x == last_p.x && p.y == last_p.y) || kerboard)
+        if (mouse || keyboard)
         {
             study_time++;
         }
         outfile << (string)temp << " ";
         outfile << study_time << "mins" << endl;
     }
-
+	outfile.close();
     return 0;
 }
